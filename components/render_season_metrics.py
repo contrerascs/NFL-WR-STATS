4305d9466a627b5d6740b64043eac0a9ed897d8a
contrerascs <60404702+contrerascs@users.noreply.github.com>
2025-06-22 21:03:35 -0600

    Season metrics

--- components/render_season_metrics.py
+++ components/render_season_metrics.py
@@ -3,4 +3,9 @@ import streamlit as st
 import pandas as pd
 
 def render_season_metrics(df,selected_season,selected_wr,wr_id):
-    pass
+    df_season = df[df["Season"] == selected_season]
+    st.title(f'{selected_wr} stats in {selected_season}')
+
+    # Mostrar estadísticas de la temporada
+    stats = ['Pts*',"Att", "Cmp", "Yds", "TD", "Int"]
+    columns = st.columns(len(stats))
