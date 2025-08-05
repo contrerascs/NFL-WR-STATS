653ec99b2cc335f1d26127d3d87b5a7c20e052df
contrerascs <60404702+contrerascs@users.noreply.github.com>
2025-08-04 22:12:02 -0600

    Render carrer metrics

--- components/render_carrer_metrics.py
+++ components/render_carrer_metrics.py
@@ -1,1 +1,31 @@
 #Render carrer metrics
+import streamlit as st
+
+def render_carrer_metrics(wr_data,selected_wr):
+    st.subheader(f'Estadisticas de {selected_wr} en toda su carrera')
+
+    c1, c2, c3, c4, c5, c6 = st.columns(6)
+    
+    with c1:
+        games = int(wr_data["GS"].sum())
+        st.metric("Partidos Jugados",f"{games:,}",border=True)
+    
+    with c2:
+        cmp_value = float(wr_data['Cmp%'].iloc[0])
+        st.metric('Cmp%',f"{round(cmp_value, 2)}%",border=True)
+    
+    with c3:
+        yards = int(wr_data["Yds"].sum())
+        st.metric("Yardas Totales",f"{yards:,}",border=True)
+    
+    with c4:
+        tds = int(wr_data["TD"].sum())
+        st.metric("Touchdowns", tds,border=True)
+    
+    with c5:
+        ints = int(wr_data["Int"].sum())
+        st.metric("Intercepciones", ints,border=True)
+    
+    with c6:
+        rating = float(wr_data['Rate'].iloc[0])
+        st.metric('Rating', value=round(rating, 2),border=True)
